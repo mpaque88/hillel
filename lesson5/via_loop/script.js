a3893903469b12a512f78c16f1758e2ef4a36f8a
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-08-14 13:54:02 +0300

    enhanced loop

--- lesson5/via_loop/script.js
+++ lesson5/via_loop/script.js
@@ -5,12 +5,13 @@ let anotherString = replaceAll(someString, 'l', 'z');
 console.log(anotherString);
 
 function replaceAll(str, inChar, outChar) {
+
 	if (inChar.length > 1 || outChar.length > 1) {
 		console.warn('Wrong input');
 	} else {
-		for (let i = 1; i < str.length; i++) {
+		do {
 			str = str.replace(inChar, outChar);
-		}
+		} while (str.indexOf(inChar) != -1);
 	}
 
 	return str;
