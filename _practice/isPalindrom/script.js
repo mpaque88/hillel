6d5eef6a4784829c73374bba2aa76f593a780210
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-08-15 15:55:11 +0300

    loop removed

--- _practice/isPalindrom/script.js
+++ _practice/isPalindrom/script.js
@@ -6,12 +6,8 @@ alert(isPalindrom(someString));
 
 function isPalindrom(str) {
 	
-	let revStr = '';
 	str = str.toLowerCase().trim();
+	let rev = str.split('').reverse().join('');
 
-	for (let i = str.length; i > 0; i--) {
-		revStr += str[i - 1];
-	}
-
-	return (str == revStr) ? true : false; 
+	return (str == rev) ? true : false; 
 }
