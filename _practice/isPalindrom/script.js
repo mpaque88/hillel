b96ac3ff632d01d529f8b43d3cadf55fae7d0129
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-08-15 14:33:07 +0300

    isPalindrom function

---
+++ _practice/isPalindrom/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let someString = prompt('Submit some string: ', 'kabak');
+alert(isPalindrom(someString));
+
+
+function isPalindrom(str) {
+	
+	let revStr = '';
+	str = str.toLowerCase().trim();
+
+	for (let i = str.length; i > 0; i--) {
+		revStr += str[i - 1];
+	}
+
+	return (str == revStr) ? true : false; 
+}
