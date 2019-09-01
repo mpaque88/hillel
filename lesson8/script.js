3962e9c884a349465f2ba3d990002e4dcd17c6f9
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-09-01 15:17:30 +0300

    add function createLi

--- lesson8/script.js
+++ lesson8/script.js
@@ -6,19 +6,17 @@ const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', newLi);
 
 function newLi() {
-    let num = +count.value;
     list.innerHTML = '';
 
-    if (num) {
-        for (let i = 1; i < num + 1; i++) {
-            let node = document.createElement('li');
-            let textNode = document.createTextNode(i);
-
-            node.appendChild(textNode);
-            node.setAttribute(`data-li-index`, i);
-
-            list.appendChild(node);
-        }
-    }
+    let num = +count.value;
+    if (num) for (let i = 1; i < num + 1; i++) createLi(i);
 }
 
+function createLi(index) {
+    let node = document.createElement('li');
+    let textNode = document.createTextNode(index);
+    
+    node.appendChild(textNode);
+    node.setAttribute(`data-li-index`, index);
+    list.appendChild(node);
+}
