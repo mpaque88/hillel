6f4138dc9ea89a12a8c8f0f16cbf5f0ca614b069
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-09-10 11:38:40 +0300

    small fix

--- lesson10_calc-via-closure/x.js
+++ lesson10_calc-via-closure/x.js
@@ -1,13 +1,11 @@
 const calculator = createCalculator(10);
 
 function createCalculator(x = 0) {
-    function value() { return x }
-
-    value.add = (y) => x += y;
-    value.sub = (y) => x -= y;
-    value.mult = (y) => x *= y;
-    value.divide = (y) => x /= y;
-    value.set = (y) => x = y;
-
-    return value;
+    return {
+        add : (y) => x += y,
+        sub : (y) => x -= y,
+        mult : (y) => x *= y,
+        div : (y) => x /= y,
+        set : (y) => x = y
+    }
 }
