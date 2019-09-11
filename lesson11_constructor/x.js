0a7485ef781c20080488f5323be6634901a3c8ad
Vadim Klyatskiy <v.klatskiy@gmail.com>
2019-09-11 10:14:39 +0300

    students arr and avr func added

--- lesson11_constructor/x.js
+++ lesson11_constructor/x.js
@@ -1,22 +1,28 @@
 'use strict'
 
+const students = [];
+const jack = new Student('Jack', [5, 4, 3]);
+const jon = new Student('Jon', [3, 2, 4]);
+
 function Student(name, marks) {
     this.name = name;
     this.marks = marks;
+    students.push(this);
 }
 
-Student.prototype.averageMarks = function(arr) {
-    arr = this.marks;
+Student.prototype.averageMark = function() { 
+    return averageOfArr(this.marks) 
+}
+
+function averageGroupMark(students) {
+    let arr = [];
+    for (let x in students) arr[x] = averageOfArr(students[x].marks); 
+    return averageOfArr(arr)
+}
+
+function averageOfArr(arr) {
     let sum = 0;
-
-    for (let key of arr) sum += key;
-
+    for (let x of arr) sum += x;
     return sum / arr.length
 }
-
-Student.prototype.sayHi = function() {
-    console.log(`Hello ${this.name}!`);
-}
-
-const jack = new Student('Jack', [5, 4, 3]);
 
