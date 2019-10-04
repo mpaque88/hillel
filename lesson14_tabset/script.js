4e180f11a97b6484dcaaa5eec1e220e2e016e21b
mpaque88 <v.klatskiy@gmail.com>
2019-10-04 10:28:27 +0300

    static properties created

--- lesson14_tabset/script.js
+++ lesson14_tabset/script.js
@@ -1,35 +1,35 @@
-const TABSET_CONTAINER = 'tab-tabset';
-const TABSET_TAB_ACTIVE = 'tab-active';
-const TABSET_TITLE = 'tab-title';
-const TABSET_TITLE_INDEX = 'data-tab-title-index';
-const TABSET_CONTENT = 'tab-content';
-const TABSET_CONTENT_INDEX = 'data-tab-content-index';
-
 class Tabset {
     constructor(el) {
         this.el = el;
         this.titleSet = this.el.children[0].children;
         this.contentSet = this.el.children[1].children;
         this.onElementClick = this.onElementClick.bind(this);
-
+        
         this.bindClasses();
         this.bindIndexes();
         this.bindEventListeners();
     }
+    
+    static TABSET_CONTAINER = 'tab-tabset';
+    static TABSET_TAB_ACTIVE = 'tab-active';
+    static TABSET_TITLE = 'tab-title';
+    static TABSET_TITLE_INDEX = 'data-tab-title-index';
+    static TABSET_CONTENT = 'tab-content';
+    static TABSET_CONTENT_INDEX = 'data-tab-content-index';
 
     bindClasses() {
-        this.el.classList.add(TABSET_CONTAINER);
-        this.el.children[0].classList.add(TABSET_TITLE);
-        this.el.children[1].classList.add(TABSET_CONTENT);
+        this.el.classList.add(Tabset.TABSET_CONTAINER);
+        this.el.children[0].classList.add(Tabset.TABSET_TITLE);
+        this.el.children[1].classList.add(Tabset.TABSET_CONTENT);
 
-        this.contentSet[0].classList.add(TABSET_TAB_ACTIVE);
-        this.titleSet[0].classList.add(TABSET_TAB_ACTIVE);
+        this.contentSet[0].classList.add(Tabset.TABSET_TAB_ACTIVE);
+        this.titleSet[0].classList.add(Tabset.TABSET_TAB_ACTIVE);
     }
 
     bindIndexes() {
         for (let i = 0; i < this.titleSet.length; i++) {
-            this.titleSet[i].setAttribute(TABSET_TITLE_INDEX, i + 1);
-            this.contentSet[i].setAttribute(TABSET_CONTENT_INDEX, i + 1);
+            this.titleSet[i].setAttribute(Tabset.TABSET_TITLE_INDEX, i + 1);
+            this.contentSet[i].setAttribute(Tabset.TABSET_CONTENT_INDEX, i + 1);
         }
     }
 
@@ -40,7 +40,7 @@ class Tabset {
     onElementClick(e) {
         if (e.target.dataset.tabTitleIndex) {
             const tabIndex = e.target.dataset.tabTitleIndex - 1;
-            const isOpened = this.contentSet[tabIndex].classList.contains(TABSET_TAB_ACTIVE);
+            const isOpened = this.contentSet[tabIndex].classList.contains(Tabset.TABSET_TAB_ACTIVE);
 
             this.hideAllTabs();
 
@@ -52,14 +52,14 @@ class Tabset {
     
     hideAllTabs() {
         for (let i = 0; i < this.contentSet.length; i++) {
-            this.contentSet[i].classList.remove(TABSET_TAB_ACTIVE);
-            this.titleSet[i].classList.remove(TABSET_TAB_ACTIVE);
+            this.contentSet[i].classList.remove(Tabset.TABSET_TAB_ACTIVE);
+            this.titleSet[i].classList.remove(Tabset.TABSET_TAB_ACTIVE);
         }
     }
     
     showTab(index) {
-        this.contentSet[index].classList.add(TABSET_TAB_ACTIVE);
-        this.titleSet[index].classList.add(TABSET_TAB_ACTIVE);
+        this.contentSet[index].classList.add(Tabset.TABSET_TAB_ACTIVE);
+        this.titleSet[index].classList.add(Tabset.TABSET_TAB_ACTIVE);
     }
 
     show(x) {
@@ -68,7 +68,7 @@ class Tabset {
     }
     
     prev() {
-        const activeTabIndex = document.querySelector(`.${TABSET_TAB_ACTIVE}`).dataset.tabTitleIndex;
+        const activeTabIndex = document.querySelector(`.${Tabset.TABSET_TAB_ACTIVE}`).dataset.tabTitleIndex;
         
         this.hideAllTabs();
         
@@ -80,7 +80,7 @@ class Tabset {
     }
     
     next() {
-        const activeTabIndex = document.querySelector(`.${TABSET_TAB_ACTIVE}`).dataset.tabTitleIndex;
+        const activeTabIndex = document.querySelector(`.${Tabset.TABSET_TAB_ACTIVE}`).dataset.tabTitleIndex;
         
         this.hideAllTabs();
         
