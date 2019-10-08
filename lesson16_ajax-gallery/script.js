c81427251da79274547ab1b1f633d4c2fd79b7b3
mpaque88 <v.klatskiy@gmail.com>
2019-10-09 00:00:32 +0300

    working proto

--- lesson16_ajax-gallery/script.js
+++ lesson16_ajax-gallery/script.js
@@ -3,37 +3,44 @@ const gallery = container.children[0];
 const background = container.children[1];
 const fullImg = container.children[2];
 const imgTemp = document.getElementById('imgTemp').innerHTML.trim();
-
-hide(container.children[1], container.children[2]);
-
-
 const prom = fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
-let photos;
+let photos = [];
 
-prom.then((resp) => {
-    resp.json().then((data) => {
-        photos = data;
+createGallery();
+bindEventListeners();
+hide(background, fullImg);
 
-        for( let i = 0; i < photos.length; i++ ) {
-            let newImg = document.createElement('div');
-            newImg.innerHTML = imgTemp.replace('{{thumbURL}}', photos[i].thumbnailUrl)
-                                      .replace('{{title}}', photos[i].title)
-                                      .replace('{{fullURL}}', photos[i].url)
-                                      .replace('{{index}}', photos[i].id);
+function bindEventListeners() {
+    gallery.addEventListener('click', showImg);
+    background.addEventListener('click', closeImg);
+}
 
-            newImg.firstElementChild.classList.add('gallery-img');
-            container.firstElementChild.append(newImg);
-        }
-    });
-});
+function createGallery(){
+    prom.then((resp) => { resp.json().then((data) => {
+            photos = data;
+            createImages(photos, container);
+        });
+    }); 
+}
 
-gallery.addEventListener('click', showImg);
-background.addEventListener('click', closeImg);
+function createImages(json, elem) {
+    for( let i = 0; i < json.length; i++ ) {
+        let newImg = document.createElement('div');
+        newImg.innerHTML = imgTemp.replace('{{thumbURL}}', json[i].thumbnailUrl)
+                                  .replace('{{title}}', json[i].title)
+                                  .replace('{{fullURL}}', json[i].url)
+                                  .replace('{{index}}', json[i].id);
+
+        newImg.firstElementChild.classList.add('gallery-img');
+        elem.firstElementChild.append(newImg);
+    }
+}
 
 function showImg(e) {
     if(e.target.classList.contains('gallery-img')) {
         background.classList.remove('hidden');
         fullImg.classList.remove('hidden');
+
         fullImg.firstElementChild.src = e.target.dataset.fullUrl;
         fullImg.firstElementChild.alt = e.target.alt;
         fullImg.firstElementChild.setAttribute('data-index', e.target.dataset.index);
@@ -55,7 +62,6 @@ function hide(...elems) {
 function closeImg() {
     hide(background, fullImg);
 }
-
 
 // - close btn
 // - next/prev btns
