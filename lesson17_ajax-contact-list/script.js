3d70cb7a15063a0d1b78e684599772d37b23deb4
mpaque88 <v.klatskiy@gmail.com>
2019-10-13 20:46:29 +0300

    v1

--- lesson17_ajax-contact-list/script.js
+++ lesson17_ajax-contact-list/script.js
@@ -1,63 +1,81 @@
-const SHORT_CONTACT_TEMP = document.getElementById('shortContactTemplate').innerHTML.trim();
-const FULL_CONTACT_TEMP = document.getElementById('fullContactTemplate').innerHTML.trim();
-const SHORT_USER_LIST_DIV = document.getElementById('list-short');
-const SHORT_USER_LIST_UL = SHORT_USER_LIST_DIV.firstElementChild;
-const USERS_DATA = 'https://jsonplaceholder.typicode.com/users';
-const USER_DATA_TEMPLATE = 'https://jsonplaceholder.typicode.com/users/{{id}}';
+const CLASS_SIDEBAR_CONTAINER = 'sidebar';
+const CLASS_SIDEBAR_LIST = 'sidebar-list';
+const CLASS_SIDEBAR_USER = 'sidebar-user';
+
+const SIDEBAR_USER_TEMPLATE = document.getElementById('sidebarUser').innerHTML.trim();
+const FULL_CONTACT_TEMPLATE = document.getElementById('fullUserName').innerHTML.trim();
+
+const SIDEBAR_CONTAINER = document.getElementById('sidebar');
+const SIDEBAR_LIST = SIDEBAR_CONTAINER.firstElementChild;
+const MAIN_CONTAINER = document.getElementById('main');
+
+const USER_DATA_URL = 'https://jsonplaceholder.typicode.com/users';
+const USER_DATA_URL_TEMPLATE = 'https://jsonplaceholder.typicode.com/users/{{id}}';
 
 let users = [];
 
-fetch(USERS_DATA)
+fetch(USER_DATA_URL)
     .then(resp => resp.json())
-    .then(data => createUserList(data, SHORT_USER_LIST_UL));
+    .then(data => {
+        createSidebar(data, SIDEBAR_LIST);
+        bindEventListeners();
+        renderUserInfo(data[0]);
+    });
 
-function createUsers(json, elem) {
+function createSidebar(json, elem) {
     createUserList(json, elem);
-    // TO DO
-    //renderUserInfo();
+    adjustClasses(elem.children, CLASS_SIDEBAR_USER);
+
+    elem.classList.add(CLASS_SIDEBAR_LIST);
+    elem.parentElement.classList.add(CLASS_SIDEBAR_CONTAINER);
 }
 
 function createUserList(json, elem) {
     let listData = json.map(item => {
-        return SHORT_CONTACT_TEMP.replace('{{id}}', item.id)
-                                 .replace('{{name}}', item.name);
+        return SIDEBAR_USER_TEMPLATE.replace('{{id}}', item.id)
+            .replace('{{name}}', item.name);
     })
 
     elem.innerHTML = listData.join(`\n`);
 }
 
+function adjustClasses(collection, addClass) {
+    Array.prototype.forEach.call(collection, item => item.classList.add(addClass))
+}
 
+function bindEventListeners() {
+    SIDEBAR_CONTAINER.addEventListener('click', onUserClick);
+}
 
+function onUserClick(e) {
+    MAIN_CONTAINER.classList.add('loading');
 
+    if (e.target.classList.contains(CLASS_SIDEBAR_USER)) {
+        fetch(USER_DATA_URL_TEMPLATE.replace('{{id}}', e.target.dataset.userId))
+            .then(resp => resp.json())
+            .then(data => {
+                renderUserInfo(data);
 
+                MAIN_CONTAINER.classList.add('visible');
+                MAIN_CONTAINER.classList.remove('loading');
+            })
+    }
+}
 
-
-
-
-
-
-// async data fetch
-
-// createTimer(3000).then(() => {
-//     console.log('alert');  
-//     return fetch(url);
-// })
-// .then((resp) => {
-//     return resp.json();
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch(() => {
-//     console.log('error');
-// })
-
-// function createTimer(miliseconds) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             (miliseconds >= 5000) ? reject() : resolve();
-//         }, miliseconds);
-//     })
-// }
-
-
+function renderUserInfo(data) {
+    MAIN_CONTAINER.innerHTML = FULL_CONTACT_TEMPLATE
+        .replace('{{city}}', data.address.city)
+        .replace('{{street}}', data.address.street)
+        .replace('{{suite}}', data.address.suite)
+        .replace('{{zipcode}}', data.address.zipcode)
+        .replace('{{lat}}', data.address.geo.lat)
+        .replace('{{lng}}', data.address.geo.lng)
+        .replace('{{companyname}}', data.company.name)
+        .replace('{{catchphrase}}', data.company.catchPhrase)
+        .replace('{{bs}}', data.company.bs)
+        .replace('{{email}}', data.email)
+        .replace('{{phone}}', data.phone)
+        .replace('{{username}}', data.username)
+        .replace('{{website}}', data.website)
+        .replace('{{name}}', data.name);
+}
