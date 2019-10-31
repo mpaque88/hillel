c88ff2788620b5bf95151d8e8be3d70e1314b075
mpaque88 <v.klatskiy@gmail.com>
2019-10-31 06:25:11 +0200

    fix

--- lesson20_sticker-board/script.js
+++ lesson20_sticker-board/script.js
@@ -10,6 +10,7 @@ const addBtn = document.getElementById('add-button');
 const stickerTemplate = document.getElementById('sticker-template').innerHTML.trim();
 
 let stickerListHtml = [];
+let stickerListItems = [];
 
 addBtn.addEventListener('click', onAddBtnClick);
 stickerContainer.addEventListener('click', onStickerClick);
@@ -17,18 +18,19 @@ stickerContainer.addEventListener('click', onStickerClick);
 init();
 
 function init() {
-    if (!localStorage.myStickers) 
+    if (localStorage.myStickers) {
+        stickerListItems = JSON.parse(localStorage.myStickers);
+    } else {
         localStorage.myStickers = [];
-      
-    renderList(localStorage.myStickers);
+        stickerListItems = [];
+    }
+    
+    renderStickersFromList(stickerListItems);
 }
 
-function renderList(json) {
-    
-    if (json) {
-        list = JSON.parse(json);
-
-        stickerListHtml = list.map((sticker) => {
+function renderStickersFromList(stickers) {
+    if (stickers) {
+        stickerListHtml = stickers.map((sticker) => {
             return stickerTemplate
                 .replace('{{id}}', sticker.id)
                 .replace('{{title}}', sticker.title)
@@ -37,64 +39,68 @@ function renderList(json) {
 
         stickerContainer.innerHTML = stickerListHtml.join('\n');
     }
-
 }
 
 function onAddBtnClick() {
     addNewStickerItem();
-    renderList(localStorage.myStickers);
+    renderStickersFromList(stickerListItems);
 }
 
 function addNewStickerItem() {
     const sticker = {
         id: Date.now(),
         title: 'Do something',
-        body: 'Some details here'
+        body: 'Some details here',
+        editMode: false
     }
 
-    if (!localStorage.myStickers) {
-        let list = [];
-        list.push(sticker);
-        localStorage.myStickers = JSON.stringify(list);
-    } else {
-        let list = getListFromJson(LOCAL_DATA_KEY);
-        list.push(sticker);
-        localStorage.myStickers = JSON.stringify(list);
-    }
+    stickerListItems.push(sticker);
+    updateLocalData(stickerListItems);
 }
 
 function onStickerClick(e) {
     const id = e.target.parentElement.dataset.stickerId;
+    const stickerItem = getListItemElement(id);
+    
 
     switch (true) {
         case e.target.classList.contains(REMOVE_BUTTON_CLASS): 
             removeSticker(id); 
             break;
-        case e.target.classList.contains(EDIT_BUTTON_CLASS): { 
-            toggleEditButtonText(e.target);
-            toggleEditMode(id);
+        case e.target.classList.contains(EDIT_BUTTON_CLASS): {
+            
+            console.dir(stickerItem.editMode);
+            if (stickerItem.editMode == true) { 
+
+
+
+
+                saveItem(stickerItem)
+
+
+
+
+            } else {
+
+
+
+
+                editModeOn(id);
+
+
+
+
+
+                
+            }
         }; break;
     }
 }
 
 function removeSticker(id) {   
-    const sticker = getListItemElement(id);
-    
-    removeStickerData(sticker.id);
-    removeElementNode(sticker.id)
-}
-
-function getListItemElement(id) {
-    const list = JSON.parse(localStorage.myStickers)
-    return list.find(item => item.id == id);
-}
-
-function removeStickerData(id) {
-    let list = getListFromJson(LOCAL_DATA_KEY);
-    const sticker = getListItemElement(id);
-
-    list = list.filter(item => item.id != sticker.id);
-    localStorage.setItem(LOCAL_DATA_KEY, JSON.stringify(list));
+    removeElementNode(id);
+    removeItemFromList(id, stickerListItems)
+    updateLocalData(stickerListItems);
 }
 
 function removeElementNode(id) {
@@ -103,49 +109,65 @@ function removeElementNode(id) {
     elem && elem.remove();
 }
 
+function removeItemFromList(id, list) {
+    let itemIndex = list.findIndex(item => item.id == id);
+
+    list.splice(itemIndex, 1);
+}
+
+
+
+function getListItemElement(id) {
+    const list = JSON.parse(localStorage.myStickers)
+    return list.find(item => item.id == id);
+}
+
 function findElementNode(id) {
     return document.querySelector(`[data-sticker-id="${id}"`);
 }
 
-function toggleEditMode(id) {
-    const stickerNode = findElementNode(id);
-    const input = stickerNode.children[0][0];           
-    const textarea = stickerNode.children[0][1];       
+
+
+
+
+
+
+
+
+function saveItem(item) {
+    updateItemInList(item, stickerListItems);
+    updateLocalData(stickerListItems)
+    renderStickersFromList(stickerListItems);
+}
+
+
+
+function editModeOn(id) {
+
     
-    if (stickerNode.classList.contains(EDIT_MODE_CLASS)) {
-        const stickerItem = getListItemElement(id);
-        stickerItem.title = input.value;
-        stickerItem.body = textarea.value;
-
-        saveSticker(stickerItem);
-        editModeOff(input, textarea, stickerNode);
-    } else {
-        editModeOn(input, textarea, stickerNode);
-    }
 }
 
-function toggleEditButtonText(btn) {
-    btn.innerHTML == 'Edit' ? btn.innerHTML = 'Save' : btn.innerHTML = 'Edit';
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateItemInList(item, list) {
+    let itemIndex = list.findIndex(el => el.id == item.id);
+
+    list[itemIndex] = item;
 }
 
-function editModeOff(title, body, container) {
-    title.disabled = true;
-    body.readOnly = true;
-    container.classList.remove(EDIT_MODE_CLASS);
-}
-
-function editModeOn(title, body, container) {
-    title.disabled = false;
-    body.readOnly = false;
-    container.classList.add(EDIT_MODE_CLASS);
-}
-
-function saveSticker(item) {
-    let list = getListFromJson(LOCAL_DATA_KEY);
-
-    list[list.findIndex(el => el.id == item.id)] = item;
-
-    localStorage.setItem(LOCAL_DATA_KEY, JSON.stringify(list));
+function updateLocalData(list) {
+    localStorage.myStickers = JSON.stringify(list);
 }
 
 function getListFromJson(key) {
